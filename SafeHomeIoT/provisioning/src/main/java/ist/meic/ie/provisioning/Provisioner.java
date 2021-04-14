9c5b643f31d45329448fd55b73b4556d22c10eea
Pedro Pereira <pedro.l.pereira@tecnico.ulisboa.pt>
2021-04-14 20:38:09 +0100

    Fixed simulator module structure.

--- SafeHomeIoT/provisioning/src/main/java/ist/meic/ie/provisioning/Provisioner.java
+++ SafeHomeIoT/provisioning/src/main/java/ist/meic/ie/provisioning/Provisioner.java
@@ -18,40 +18,55 @@ public class Provisioner{
         this.dbConfig= new DatabaseConfig("mytestdb2.cwoffguoxxn0.us-east-1.rds.amazonaws.com", "HLR", "storemessages", "enterpriseintegration2021");
     }
 
-    public activateSIMCard(){//insert into db new SIMCARD
+    public void activateSIMCard(){//insert into db new SIMCARD
         PreparedStatement activation;
-        activation = dbConfig.getConnection.prepareStatement ("insert into activeSubscriber (SIMCARD,MSISDN,subType) values(?,?,?)");
-        activation.setString(1,this.SIMCARD);
-        activation.setString(2,this.MSISDN);
-        activation.setString(3,this.subType);
-        activation.executeUpdate();
-        activation.close();
+        try {
+            activation = dbConfig.getConnection().prepareStatement ("insert into activeSubscriber (SIMCARD,MSISDN,subType) values(?,?,?)");
+            activation.setString(1,this.SIMCARD);
+            activation.setString(2,this.MSISDN);
+            activation.setString(3,this.subType);
+            activation.executeUpdate();
+            activation.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 
-    public suspendSIMCard(){
+    public void suspendSIMCard() {
         PreparedStatement deleteActive;
         PreparedStatement insertSuspend;
-        deleteActive = dbConfig.getConnection.prepareStatement ("delete from activeSubscriber where SIMCARD=? and MSISDN=?");
-        deleteActive.setString(1,this.SIMCARD);
-        deleteActive.setString(2,this.MSISDN);
-        insertSuspend = dbConfig.getConnection.prepareStatement ("insert into suspendedSubscriber (SIMCARD,MSISDN,subType) values(?,?,?)");
-        insertSuspend.setString(1,this.SIMCARD);
-        insertSuspend.setString(2,this.MSISDN);
-        insertSuspend.setString(3,this.subType);
-        deleteActive.executeUpdate();
-        insertSuspend.executeUpdate();
-        deleteActive.close();
-        insertSuspend.close();
+        try {
+            deleteActive = dbConfig.getConnection().prepareStatement ("delete from activeSubscriber where SIMCARD=? and MSISDN=?");
+            deleteActive.setString(1,this.SIMCARD);
+            deleteActive.setString(2,this.MSISDN);
+            insertSuspend = dbConfig.getConnection().prepareStatement ("insert into suspendedSubscriber (SIMCARD,MSISDN,subType) values(?,?,?)");
+            insertSuspend.setString(1,this.SIMCARD);
+            insertSuspend.setString(2,this.MSISDN);
+            insertSuspend.setString(3,this.subType);
+            deleteActive.executeUpdate();
+            insertSuspend.executeUpdate();
+            deleteActive.close();
+            insertSuspend.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 
-    public deleteSIMCard(){
+    public void deleteSIMCard() {
         PreparedStatement deleteActive;
-        deleteActive = dbConfig.getConnection.prepareStatement ("delete from activeSubscriber where SIMCARD=? and MSISDN=?");
-        deleteActive.setString(1,this.SIMCARD);
-        deleteActive.setString(2,this.MSISDN);
+        try {
+            deleteActive = dbConfig.getConnection().prepareStatement ("delete from activeSubscriber where SIMCARD=? and MSISDN=?");
+            deleteActive.setString(1,this.SIMCARD);
+            deleteActive.setString(2,this.MSISDN);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 
-    public pingSIMCard(){
+    public void pingSIMCard(){
 
     }
 }

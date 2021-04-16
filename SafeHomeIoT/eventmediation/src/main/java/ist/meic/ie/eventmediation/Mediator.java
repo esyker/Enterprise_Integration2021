85e0c2e88bd5ce9285037e1093dce7266f5bca20
Pedro Pereira <pedro.l.pereira@tecnico.ulisboa.pt>
2021-04-16 21:25:28 +0100

    Fixed bug with group id. Deploy.

--- SafeHomeIoT/eventmediation/src/main/java/ist/meic/ie/eventmediation/Mediator.java
+++ SafeHomeIoT/eventmediation/src/main/java/ist/meic/ie/eventmediation/Mediator.java
@@ -31,7 +31,7 @@ public class Mediator {
         }
         System.out.println(topics);
 
-        KafkaConsumer<String, String> userManagerConsumer = KafkaConfig.createKafkaConsumer(cmd.getOptionValue("kafkaip"), "mediator", Collections.singletonList("new-user-events"));
+        KafkaConsumer<String, String> userManagerConsumer = KafkaConfig.createKafkaConsumer(cmd.getOptionValue("kafkaip"), "user-events", Collections.singletonList("new-user-events"));
         KafkaConsumer<String, String> consumer = KafkaConfig.createKafkaConsumer(cmd.getOptionValue("kafkaip"), "mediator", topics);
         DatabaseConfig config = new DatabaseConfig("events-2.cq2nyt0kviyb.us-east-1.rds.amazonaws.com", "SafeHomeIoTEvents", "pedro", "123456789");
         while (true) {
